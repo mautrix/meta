90c72636fac05d7c182e2e085a1ffe940c414604
JJTech0130 <jjtech@jjtech.dev>
2024-06-29 19:25:56 -0400

    incorporate Tulir's suggestions

--- pkg/connector/login.go
+++ pkg/connector/login.go
@@ -6,17 +6,19 @@ import (
 
 	"maunium.net/go/mautrix/bridgev2"
 
+	"go.mau.fi/util/exslices"
+
 	"go.mau.fi/mautrix-meta/messagix"
 	"go.mau.fi/mautrix-meta/messagix/cookies"
 	"go.mau.fi/mautrix-meta/messagix/table"
 	"go.mau.fi/mautrix-meta/messagix/types"
 )
 
-var FACEBOOK_COOKIES_FLOW = "cookies-facebook"
-var INSTAGRAM_COOKIES_FLOW = "cookies-instagram"
+const FlowIDFacebookCookies = "cookies-facebook"
+const FlowIDInstagramCookies = "cookies-instagram"
 
 func (m *MetaConnector) CreateLogin(ctx context.Context, user *bridgev2.User, flowID string) (bridgev2.LoginProcess, error) {
-	if flowID != FACEBOOK_COOKIES_FLOW && flowID != INSTAGRAM_COOKIES_FLOW {
+	if flowID != FlowIDFacebookCookies && flowID != FlowIDInstagramCookies {
 		return nil, fmt.Errorf("unknown flow ID %s", flowID)
 	}
 
@@ -31,14 +33,14 @@ func (m *MetaConnector) GetLoginFlows() []bridgev2.LoginFlow {
 	facebook := bridgev2.LoginFlow{
 		Name:        "Facebook Cookies",
 		Description: "Login using cookies from Facebook Messenger",
-		ID:          FACEBOOK_COOKIES_FLOW,
+		ID:          FlowIDFacebookCookies,
 	}
 	instagram := bridgev2.LoginFlow{
 		Name:        "Instagram Cookies",
 		Description: "Login using cookies from Instagram",
-		ID:          INSTAGRAM_COOKIES_FLOW,
+		ID:          FlowIDInstagramCookies,
 	}
-	if m.Config == nil {
+	if m.Config == nil || m.Config.Mode == "" {
 		return []bridgev2.LoginFlow{facebook, instagram}
 	} else if m.Config.Mode == "facebook" {
 		return []bridgev2.LoginFlow{facebook}
@@ -57,17 +59,8 @@ type MetaCookieLogin struct {
 
 var _ bridgev2.LoginProcessCookies = (*MetaCookieLogin)(nil)
 
-func requiredCookiesToKeys(requiredCookies []cookies.MetaCookieName) []string {
-	var keys []string
-	// Turn the array of MetaCookieName into an array of strings
-	for _, key := range requiredCookies {
-		keys = append(keys, string(key))
-	}
-	return keys
-}
-
 func (m *MetaCookieLogin) Start(ctx context.Context) (*bridgev2.LoginStep, error) {
-	if m.Flow == FACEBOOK_COOKIES_FLOW {
+	if m.Flow == FlowIDFacebookCookies {
 		return &bridgev2.LoginStep{
 			Type:         bridgev2.LoginStepTypeCookies,
 			StepID:       "fi.mau.meta.cookies",
@@ -76,10 +69,10 @@ func (m *MetaCookieLogin) Start(ctx context.Context) (*bridgev2.LoginStep, error
 				URL:          "https://www.facebook.com/",
 				UserAgent:    messagix.UserAgent,
 				CookieDomain: "www.facebook.com",
-				CookieKeys:   requiredCookiesToKeys(cookies.FBRequiredCookies),
+				CookieKeys:   exslices.CastToString[string](cookies.FBRequiredCookies),
 			},
 		}, nil
-	} else if m.Flow == INSTAGRAM_COOKIES_FLOW {
+	} else if m.Flow == FlowIDInstagramCookies {
 		return &bridgev2.LoginStep{
 			Type:         bridgev2.LoginStepTypeCookies,
 			StepID:       "fi.mau.meta.cookies",
@@ -88,7 +81,8 @@ func (m *MetaCookieLogin) Start(ctx context.Context) (*bridgev2.LoginStep, error
 				URL:          "https://www.instagram.com/",
 				UserAgent:    messagix.UserAgent,
 				CookieDomain: "www.instagram.com",
-				CookieKeys:   requiredCookiesToKeys(cookies.IGRequiredCookies),
+				//CookieKeys:   requiredCookiesToKeys(cookies.IGRequiredCookies),
+				CookieKeys: exslices.CastToString[string](cookies.IGRequiredCookies),
 			},
 		}, nil
 	} else {
@@ -123,7 +117,7 @@ func (m *MetaCookieLogin) SubmitCookies(ctx context.Context, strCookies map[stri
 	c := &cookies.Cookies{
 		Platform: types.Instagram,
 	}
-	if m.Flow == FACEBOOK_COOKIES_FLOW {
+	if m.Flow == FlowIDFacebookCookies {
 		c.Platform = types.Facebook
 	}
 
