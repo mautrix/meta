ec31f40e3a4f555489ab638db70d7358a4386984
Tulir Asokan <tulir@maunium.net>
2024-08-21 00:09:14 +0300

    msgconv: remove unused UpdateAvatar function

--- pkg/msgconv/media.go
+++ pkg/msgconv/media.go
@@ -23,14 +23,10 @@ import (
 	"io"
 	"net"
 	"net/http"
-	"net/url"
-	"path"
 	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
-	"maunium.net/go/mautrix"
-	"maunium.net/go/mautrix/id"
 
 	"go.mau.fi/mautrix-meta/pkg/messagix"
 )
@@ -171,68 +167,4 @@ func downloadMedia(ctx context.Context, mime, url string, maxSize int64, byteRan
 		zerolog.Ctx(ctx).Debug().Int("data_length", len(respData)).Msg("Media download complete")
 		return respData, nil
 	}
-}
-
-func UpdateAvatar(
-	ctx context.Context,
-	newAvatarURL string,
-	avatarID *string, avatarSet *bool, avatarURL *id.ContentURI,
-	uploadAvatar func(context.Context, []byte, string) (*mautrix.RespMediaUpload, error),
-	setAvatar func(context.Context, id.ContentURI) error,
-) bool {
-	log := zerolog.Ctx(ctx)
-	var newAvatarID string
-	if newAvatarURL != "" {
-		parsedAvatarURL, _ := url.Parse(newAvatarURL)
-		newAvatarID = path.Base(parsedAvatarURL.Path)
-	}
-	if *avatarID == newAvatarID && (*avatarSet || setAvatar == nil) {
-		return false
-	}
-	*avatarID = newAvatarID
-	*avatarSet = false
-	*avatarURL = id.ContentURI{}
-	if newAvatarID == "" {
-		if setAvatar == nil {
-			return true
-		}
-		err := setAvatar(ctx, *avatarURL)
-		if err != nil {
-			log.Err(err).Msg("Failed to remove avatar")
-			return true
-		}
-		log.Debug().Msg("Avatar removed")
-		*avatarSet = true
-		return true
-	}
-	avatarData, err := DownloadMedia(ctx, "image/*", newAvatarURL, 5*1024*1024)
-	if err != nil {
-		log.Err(err).
-			Str("avatar_id", newAvatarID).
-			Msg("Failed to download new avatar")
-		return true
-	}
-	avatarContentType := http.DetectContentType(avatarData)
-	resp, err := uploadAvatar(ctx, avatarData, avatarContentType)
-	if err != nil {
-		log.Err(err).
-			Str("avatar_id", newAvatarID).
-			Msg("Failed to upload new avatar")
-		return true
-	}
-	*avatarURL = resp.ContentURI
-	if setAvatar == nil {
-		return true
-	}
-	err = setAvatar(ctx, *avatarURL)
-	if err != nil {
-		log.Err(err).Msg("Failed to update avatar")
-		return true
-	}
-	log.Debug().
-		Str("avatar_id", newAvatarID).
-		Stringer("avatar_mxc", resp.ContentURI).
-		Msg("Avatar updated successfully")
-	*avatarSet = true
-	return true
 }
