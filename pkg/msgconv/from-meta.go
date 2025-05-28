e70275da2eb74b96ee2b7a5b49305ef9d6cbd3e1
Tulir Asokan <tulir@maunium.net>
2025-05-28 17:49:36 +0300

    msgconv/from-meta: fix missing size when using direct media

--- pkg/msgconv/from-meta.go
+++ pkg/msgconv/from-meta.go
@@ -769,6 +769,7 @@ func (mc *MessageConverter) reuploadAttachment(
 		if err != nil {
 			return nil, err
 		}
+		content.Info.Size = fileSize
 		fillMetadata()
 		return &bridgev2.ConvertedMessagePart{
 			Type:    event.EventMessage,
