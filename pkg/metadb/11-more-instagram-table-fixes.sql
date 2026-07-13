b1655c3b311d94b9cc30f74a01532be7ce0e2e64
Tulir Asokan <tulir@maunium.net>
2026-07-13 23:43:39 +0300

    metadb: drop tables for v11 instead of migrating data

--- pkg/metadb/11-more-instagram-table-fixes.sql
+++ pkg/metadb/11-more-instagram-table-fixes.sql
@@ -1,6 +1,8 @@
 -- v11 (compatible with v11+): Fix Instagram chat and thread ID tables again
+DROP TABLE meta_instagram_chat_id;
+DROP TABLE meta_instagram_thread_id;
 
-CREATE TABLE new_meta_instagram_thread_id (
+CREATE TABLE meta_instagram_thread_id (
     bridge_id TEXT   NOT NULL,
     igid      TEXT   NOT NULL,
     fbid      BIGINT NOT NULL,
@@ -12,7 +14,7 @@ CREATE TABLE new_meta_instagram_thread_id (
         REFERENCES user_login (bridge_id, id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE new_meta_instagram_chat_id (
+CREATE TABLE meta_instagram_chat_id (
     bridge_id TEXT   NOT NULL,
     igid      TEXT   NOT NULL,
     fbid      BIGINT NOT NULL,
@@ -23,15 +25,3 @@ CREATE TABLE new_meta_instagram_chat_id (
     CONSTRAINT meta_ig_chat_user_login_fkey FOREIGN KEY (bridge_id, login)
         REFERENCES user_login (bridge_id, id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-INSERT INTO new_meta_instagram_thread_id (bridge_id, igid, fbid, login)
-SELECT '', igid, fbid, login FROM meta_instagram_thread_id;
-
-INSERT INTO new_meta_instagram_chat_id (bridge_id, igid, fbid, login)
-SELECT '', igid, fbid, login FROM meta_instagram_chat_id;
-
-DROP TABLE meta_instagram_chat_id;
-DROP TABLE meta_instagram_thread_id;
-
-ALTER TABLE new_meta_instagram_chat_id RENAME TO meta_instagram_chat_id;
-ALTER TABLE new_meta_instagram_thread_id RENAME TO meta_instagram_thread_id;
