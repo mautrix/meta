a533d2c3067a0d081090583489b6e9c3b7d88468
Ping Chen <remotenonsense@gmail.com>
2026-07-21 22:48:32 +0900

    Update pkg/metadb/00-latest-schema.sql
    Co-authored-by: Tulir Asokan <tulir@maunium.net>

--- pkg/metadb/00-latest-schema.sql
+++ pkg/metadb/00-latest-schema.sql
@@ -1,4 +1,4 @@
--- v0 -> v12 (compatible with v12+): Latest schema
+-- v0 -> v12 (compatible with v11+): Latest schema
 CREATE TABLE meta_thread (
     parent_key BIGINT NOT NULL,
     thread_key BIGINT NOT NULL,
