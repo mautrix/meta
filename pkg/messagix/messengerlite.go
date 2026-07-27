fc6efbce392bacb1c2d7c11cd0a16b0383a088e3
Radon Rosborough <radon@beeper.com>
2026-07-27 09:55:17 -0700

    messagix/bloks: Revert mistake

--- pkg/messagix/messengerlite.go
+++ pkg/messagix/messengerlite.go
@@ -227,5 +227,5 @@ func (m *MessengerLiteMethods) DoLoginSteps(ctx context.Context, userInput map[s
 		)
 	}
 
-	return nil, nil, fmt.Errorf("login succeeded, but aborting for debugging convenience")
+	return nil, m.convertCookies(&loginRespPayload), nil
 }
