8a5218eff9073a26fc4ddb4c47f6d048da8de99e
Radon Rosborough <radon@beeper.com>
2026-08-24 15:20:26 -0700

    messagix/bloks/cmd: various features
    New docstring explains them.

--- pkg/messagix/bloks/cmd_new/main.go
+++ pkg/messagix/bloks/cmd_new/main.go
@@ -28,6 +28,39 @@ var flagIsAndroid = flag.Bool("android", false, "Use Android client instead of i
 var flagUserInput = flag.String("user-input", "", "Login step user input key/value json")
 var flagReplayLog = flag.String("replay", "", "Log file to replay Bloks responses from")
 
+// Starting state
+var flagPayload = flag.String("payload", "", "Bloks payload filename (page or action)")
+var flagDoAction = flag.Bool("action", false, "Execute the action (e.g. to display an initial page)")
+var flagStartingState = flag.String("state", "", "Browser state name (also needs -payload)")
+
+// What to do
+var flagDoPerform = flag.Bool("perform", false, "Perform login actions")
+var flagDoPrint = flag.Bool("print", false, "Pretty-print the Bloks payload")
+var flagDoHTML = flag.Bool("html", false, "Print an HTML version of the Bloks payload")
+
+// Q: I have a minified bloks payload from logs, and want it
+// pretty-printed.
+//
+// A: Use -payload <file> -print or -payload <file> -html.
+//
+// Q: I have the logs of an entire bridge login session, which fails
+// at some point. I want to reproduce the error and then test my fix.
+//
+// A: Use -replay <file> -perform -user-input <json>. You can add the
+// json incrementally as the tool will report which field(s) are
+// missing.
+//
+// Q: I have a minified bloks payload from logs, the bridge fails to
+// handle its contents correctly. I want to reproduce the error and
+// then test my fix. I only want to run code starting from that
+// specific screen.
+//
+// A: If the bloks payload is type=page, then use -payload <file>
+// -state <val> -perform. If it's type=action, then add -action too.
+// You need to specify the state enum string from the selenium module,
+// that corresponds to processing your page. This may also be printed
+// in bridge logs.
+
 func main() {
 	err := mainE()
 	if err != nil {
@@ -46,6 +79,19 @@ func readFile(filename string) ([]byte, error) {
 	}
 	defer file.Close()
 	return io.ReadAll(file)
+}
+
+func readBloksPayloadFile(filename string) (*bloks.BloksBundle, error) {
+	fileB, err := readFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var data bloks.BloksBundle
+	err = json.Unmarshal(fileB, &data)
+	if err != nil {
+		return nil, fmt.Errorf("parse: %w", err)
+	}
+	return &data, nil
 }
 
 var logsRespGzRegexp = regexp.MustCompile(`"resp_gz":"([^"]+)"`)
@@ -114,13 +160,17 @@ func mainE() error {
 		}
 	}
 
-	b, err := bloks.NewBrowser(&bloks.BrowserConfig{
+	var b *bloks.Browser
+	b, err = bloks.NewBrowser(&bloks.BrowserConfig{
 		Platform: plat,
 		EncryptPassword: func(ctx context.Context, password string) (string, error) {
 			return fmt.Sprintf(`#PWD_TEST_UNENCRYPTED:%s`, password), nil
 		},
 		MakeBloksRequest: func(ctx context.Context, doc *bloks.BloksDoc, appID string, inner bloks.BloksParamsInner, deviceID string, familyDeviceID string) (*bloks.BloksBundle, error) {
-			log.Debug().Str("bloks_app", appID).Msg("Making Bloks request")
+			if appID == "initial_action" {
+				return b.CurrentPage, nil
+			}
+			log.Debug().Str("bloks_app", appID).Any("params", inner).Msg("Making Bloks request")
 			if prepared := replayLog[appID]; prepared != nil {
 				replayLog[appID] = prepared[1:]
 				var respInner bloks.BloksBundle
@@ -130,6 +180,9 @@ func mainE() error {
 				}
 				return &respInner, nil
 			}
+			if *flagReplayLog == "" {
+				return nil, fmt.Errorf("missing bloks response (provide with -replay): %s", appID)
+			}
 			return nil, fmt.Errorf("missing bloks response: %s", appID)
 		},
 	})
@@ -137,21 +190,76 @@ func mainE() error {
 		return err
 	}
 
-	availableUserInput := map[string]string{}
-	for b.State != bloks.StateSuccess {
-		step, err := b.DoLoginStep(ctx, availableUserInput)
+	if *flagPayload != "" {
+		data, err := readBloksPayloadFile(*flagPayload)
 		if err != nil {
 			return err
 		}
-		if step != nil && step.UserInputParams != nil {
-			for _, input := range step.UserInputParams.Fields {
-				if provided := userInput[input.ID]; provided != "" {
-					availableUserInput[input.ID] = provided
-				} else {
-					return fmt.Errorf("missing user input: %s", input.ID)
+		b.CurrentPage = data
+
+		err = b.CurrentPage.SetupInterpreter(ctx, b.Bridge, nil, true)
+		if err != nil {
+			return err
+		}
+	}
+	if *flagStartingState != "" {
+		b.State = bloks.BrowserState(*flagStartingState)
+	}
+
+	if *flagDoAction {
+		b.Bridge.DoActionRPC(ctx, "initial_action", nil)
+	}
+
+	doneSomething := false
+
+	if *flagDoPrint {
+		doneSomething = true
+		if b.CurrentPage == nil {
+			return fmt.Errorf("no payload to print (provide with -payload)")
+		}
+		b.CurrentPage.Print(os.Stdout, "")
+	}
+	if *flagDoHTML {
+		doneSomething = true
+		if b.CurrentPage == nil {
+			return fmt.Errorf("no payload to print (provide with -payload)")
+		}
+		b.CurrentPage.PrintHTML(os.Stdout, "")
+	}
+	if *flagDoPerform {
+		doneSomething = true
+		if *flagPayload != "" && *flagStartingState == "" {
+			return fmt.Errorf("-perform with -payload needs -state too")
+		}
+		availableUserInput := map[string]string{}
+		for b.State != bloks.StateSuccess {
+			step, err := b.DoLoginStep(ctx, availableUserInput)
+			if err != nil {
+				return err
+			}
+			if step != nil && step.UserInputParams != nil {
+				for _, input := range step.UserInputParams.Fields {
+					if provided := userInput[input.ID]; provided != "" {
+						availableUserInput[input.ID] = provided
+					} else {
+						return fmt.Errorf("missing user input: %s", input.ID)
+					}
+				}
+			}
+			if step != nil && step.CookiesParams != nil {
+				for _, input := range step.CookiesParams.Fields {
+					if provided := userInput[input.ID]; provided != "" {
+						availableUserInput[input.ID] = provided
+					} else {
+						return fmt.Errorf("missing user input: %s", input.ID)
+					}
 				}
 			}
 		}
+	}
+
+	if !doneSomething {
+		fmt.Println("no errors, but you may want -perform or -print or -html")
 	}
 
 	return nil
